0ffdde386f4219ab4b7830d537fe913555632588
Christopher Allen <cpcallen+git@google.com>
2017-06-16 11:31:54 +0100

    Update URL in copyright statement

--- recequal_test.go
+++ recequal_test.go
@@ -1,5 +1,5 @@
 /* Copyright 2017 Google Inc.
- * https://github.com/NeilFraser/CodeCity
+ * https://github.com/cpcallen/testutil
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
